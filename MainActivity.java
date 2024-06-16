9314d6e1ef44d1eac0ad78a377874f7b55c056dc
Atiqur Rahman <86587468+Atiqur1@users.noreply.github.com>
2024-06-16 22:31:35 +0600

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.guessthesnake;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button startGame;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        startGame = findViewById(R.id.button);
+
+        startGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+}
---
+++ QuestionAnswer.java
@@ -0,0 +1,64 @@
+package com.example.guessthesnake;
+
+
+
+public class QuestionAnswer {
+
+    public static String question []={
+            "Stored Joytrip more than a Pharmacy?",
+            "Always Formal?",
+            "Study more than Love ?",
+            "Loyal to many Girls ?",
+            "Always in heat?",
+            "Gazipur's Magpie?",
+            "Guess The Snake?",
+            "Room Temperature ?",
+            "Dragon ?",
+            "Male Squab?",
+            "Female Squab?",
+            "Having Two Types of Burkha: Party & Normal?",
+            "Universal Bro?",
+            "Good Boy?",
+            "Use Me?",
+            "Take a picture of me plz?"
+
+
+
+    };
+    public static String choices [][]={
+            { "Kanak" , "Antu" , "Jobayer"},
+            {"Shoumik" , "Tial" , "Sohel" },
+            {"Sabbir" , "Mahadi" , "Shoumik" },
+            { "Rishad" , "Prithvi" , "Moin"},
+            {"Sadik" , "Azizul" , "Rabbi"},
+            {"Nayeem" , "Robin" , "Tanjima"},
+            {"Tial" , "Robin" , "Sohel"},
+            {"Nidhi" , " Shifa" , " Sweety"},
+            {"Manna", "Moin" , " Rabbi"},
+            {"Sabbir", " Mominul", "Jim"},
+            {"Tanjina", "Mahima" , "Mim"},
+            {"Lubna", "Marwa", "Nidhi"},
+            {"Samia", "Shovnom" , "Sumaiya"},
+            {"Atiq", "Atiq" , "Atiq"},
+            {"Prithvi", "Azizul", "Jim"},
+            {"Shifa", "Sweety", "Samia"}
+    };
+    public static String correctAnswers []={
+            "Antu" ,
+            "Shoumik",
+            "Mahadi",
+            "Rishad",
+            "Sadik",
+            "Robin",
+            "Sohel",
+            "Nidhi",
+            "Rabbi",
+            "Sabbir",
+            "Mahima",
+            "Marwa",
+            "Samia",
+            "Atiq",
+            "Azizul",
+            "Sweety"
+    };
+}
